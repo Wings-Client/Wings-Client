6922de7779a777d873366139f5eab44bbfda3a36
Smaltin <sterling.maltin@gmail.com>
2021-06-08 21:11:09 -0700

    I got the icon button working for now.

--- WingsClient/Main.cs
+++ WingsClient/Main.cs
@@ -1,7 +1,39 @@
-﻿namespace WingsClient
+﻿using System;
+using System.Collections.Generic;
+using Il2CppSystem.Collections;
+using Il2CppSystem.IO;
+using MelonLoader;
+using RubyButtonAPI;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace WingsClient
+
 {
-    public class Main
+    public class WingsClient : MelonMod
+
     {
-        
+        private QMNestedButton menuButton;
+
+        public override void OnApplicationStart()
+        {
+            if (!Directory.Exists("WingsClient/Texture"))
+            {
+                Directory.CreateDirectory("WingsClient/Texture");
+            }
+        }
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            InitButtons();
+        }
+
+        private void InitButtons()
+        {
+            menuButton = new QMNestedButton("ShortcutMenu", 5, -1, "", "Wings Client Menu");
+            Utils.SetImage(menuButton.getMainButton().getGameObject().GetComponent<Image>(),
+                "WingsClient/textures/icon.png");
+        }
     }
 }
---
+++ WingsClient/Utils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Il2CppSystem.Collections.Generic;
+using Il2CppSystem.IO;
+using Il2CppSystem.Security.Cryptography;
+using Il2CppSystem.Text;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC;
+
+namespace WingsClient
+{
+    public class Utils
+    {
+        private static Action<Player> requestedAction;
+
+        public static string SHA256(string unencrypted)
+        {
+            HashAlgorithm hashAlgorithm = new SHA256Managed();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte b in hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(unencrypted)))
+            {
+                stringBuilder.Append(b.ToString("x2"));
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static void SelectPlayer(Player user)
+        {
+            QuickMenu.prop_QuickMenu_0.Method_Public_Void_Player_PDM_0(user);
+        }
+
+        public static void GetEachPlayer(Action<Player> act)
+        {
+            Utils.requestedAction = act;
+            foreach (Player obj in PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0)
+            {
+                Utils.requestedAction(obj);
+            }
+        }
+
+        public static void SetImage(Image img, string path)
+        {
+            try
+            {
+                Texture2D texture2D = new Texture2D(2, 2);
+                if (Il2CppImageConversionManager.LoadImage(texture2D, File.ReadAllBytes(path)))
+                {
+                    Sprite sprite = Sprite.CreateSprite(texture2D,
+                        new Rect(0f, 0f, (float) texture2D.width, (float) texture2D.height), new Vector2(0.5f, 0.5f),
+                        100f, 0U, 0, default(Vector4), false);
+                    img.sprite = sprite;
+                }
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Msg(ex);
+            }
+        }
+    }
+}
