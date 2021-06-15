c63bb16efed793799d7441a1071b8e7a96320782
Literal <ps3g4mer@gmail.com>
2021-06-15 17:00:53 -0500

    FPSUnlocker
    It is hidden so i can figure out how to do it

---
+++ WingsClient/Modules/FPSUnlocked.cs
@@ -0,0 +1,14 @@
+﻿namespace WingsClient.Modules
+{
+    public class FPSUnlocked
+    {
+        public static void Initialize()
+        {
+            if (Configuration.JSONConfig.UnlimitedFPSEnabled)
+            {
+                Application.targetFrameRate = Shared.settings.GetSetting("FPSLimit", "60");
+            }
+        }
+    }
+}
+    
